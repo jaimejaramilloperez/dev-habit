f1b0ecc6d4a515fd0ceea4e249a666c60e032005
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2025-05-11 16:15:58 -0500

    refactor: streamline Program.cs by delegating service registrations to DependencyInjectionExtensions

--- src/DevHabit.Api/Program.cs
+++ src/DevHabit.Api/Program.cs
@@ -1,78 +1,15 @@
 using DevHabit.Api;
 using DevHabit.Api.Database;
 using DevHabit.Api.Extensions;
-using DevHabit.Api.Middlewares;
-using DevHabit.Api.Services;
-using FluentValidation;
 using HealthChecks.UI.Client;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Newtonsoft.Json.Serialization;
-using Npgsql;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseKestrel(option => option.AddServerHeader = false);
-
-builder.Host.UseDefaultServiceProvider((context, options) =>
-{
-    options.ValidateScopes = true;
-    options.ValidateOnBuild = true;
-});
-
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true;
-})
-.AddNewtonsoftJson(options =>
-{
-    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-})
-.AddXmlSerializerFormatters();
-
-builder.Services.AddValidatorsFromAssemblyContaining<IApiMarker>();
-
-builder.Services.AddProblemDetails(options => options.CustomizeProblemDetails = context =>
-{
-    context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-});
-
-builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options
-    .UseNpgsql(builder.Configuration.GetConnectionString("Database"), npgsqlOptions =>
-        npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
-    .UseSnakeCaseNamingConvention());
-
-builder.Services.AddHealthChecks()
-    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
-
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-    .WithTracing(tracing => tracing
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddNpgsql())
-    .WithMetrics(metrics => metrics
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddRuntimeInstrumentation())
-    .UseOtlpExporter();
-
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.IncludeScopes = true;
-    options.IncludeFormattedMessage = true;
-});
-
-builder.Services.AddTransient<IDataShapingService, DataShapingService>();
+builder.AddControllers();
+builder.AddErrorHandling();
+builder.AddDatabase();
+builder.AddObservability();
+builder.AddApplicationServices();
 
 var app = builder.Build();
 
