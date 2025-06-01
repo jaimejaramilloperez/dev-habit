0b1a8966705712323c2e61629011675edbfe9dcf
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2025-06-01 18:54:45 -0500

    ref: Improve formatting of validation rules in Habits and Tags parameters validators

--- src/DevHabit.Api/Dtos/Habits/HabitsParametersValidator.cs
+++ src/DevHabit.Api/Dtos/Habits/HabitsParametersValidator.cs
@@ -6,10 +6,12 @@ public sealed class HabitsParametersValidator : AbstractValidator<HabitsParamete
 {
     public HabitsParametersValidator()
     {
-        RuleFor(x => x.Page).GreaterThan(0)
+        RuleFor(x => x.Page)
+            .GreaterThan(0)
             .WithMessage("Page must be greater than 0");
 
-        RuleFor(x => x.PageSize).InclusiveBetween(1, 50)
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 50)
             .WithMessage("Page size must be between 1 and 50");
     }
 }
--- src/DevHabit.Api/Dtos/Tags/TagsParametersValidator.cs
+++ src/DevHabit.Api/Dtos/Tags/TagsParametersValidator.cs
@@ -6,10 +6,12 @@ public sealed class TagsParametersValidator : AbstractValidator<TagsParameters>
 {
     public TagsParametersValidator()
     {
-        RuleFor(x => x.Page).GreaterThan(0)
+        RuleFor(x => x.Page)
+            .GreaterThan(0)
             .WithMessage("Page must be greater than 0");
 
-        RuleFor(x => x.PageSize).InclusiveBetween(1, 50)
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 50)
             .WithMessage("Page size must be between 1 and 50");
     }
 }
