6215856bf7b3c146e3f3f6feac02536eab667e05
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2025-06-07 09:56:26 -0500

    feat: Change ScanIntervalInMinutes property to use init accessor for better immutability

--- src/DevHabit.Api/Configurations/GitHubAutomationOptions.cs
+++ src/DevHabit.Api/Configurations/GitHubAutomationOptions.cs
@@ -3,5 +3,5 @@ namespace DevHabit.Api.Configurations;
 public sealed class GitHubAutomationOptions
 {
     public const string SectionName = "Jobs";
-    public int ScanIntervalInMinutes { get; set; }
+    public int ScanIntervalInMinutes { get; init; }
 }
