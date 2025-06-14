f2dd64eaf4f239277dd95cfba39d0999b61f3785
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2025-06-14 11:53:49 -0500

    refactor: Rearrange route classes and consolidate User and Tag routes

--- test/DevHabit.IntegrationTests/Infrastructure/Routes.cs
+++ test/DevHabit.IntegrationTests/Infrastructure/Routes.cs
@@ -11,6 +11,21 @@ public static class Routes
         public const string Refresh = $"{Base}/auth/refresh";
     }
 
+    public static class EntryImportJobRoutes
+    {
+        public const string GetAll = $"{Base}/entries/imports";
+        public const string Get = $"{Base}/entries/imports";
+        public const string Create = $"{Base}/entries/imports";
+    }
+
+    public static class GitHubRoutes
+    {
+        public const string GetProfile = $"{Base}/github/profile";
+        public const string GetEvents = $"{Base}/github/events";
+        public const string StorePersonalAccessToken = $"{Base}/github/personal-access-token";
+        public const string RevokePersonalAccessToken = $"{Base}/github/personal-access-token";
+    }
+
     public static class HabitRoutes
     {
         public const string GetAll = $"{Base}/habits";
@@ -21,17 +36,18 @@ public static class Routes
         public const string Delete = $"{Base}/habits";
     }
 
+    public static class TagRoutes
+    {
+        public const string GetAll = $"{Base}/tags";
+        public const string Get = $"{Base}/tags";
+        public const string Create = $"{Base}/tags";
+        public const string Update = $"{Base}/tags";
+        public const string Delete = $"{Base}/tags";
+    }
+
     public static class UserRoutes
     {
         public const string CurrentUser = $"{Base}/users/me";
         public const string UpdateProfile = $"{Base}/users/me/profile";
-    }
-
-    public static class GitHubRoutes
-    {
-        public const string GetProfile = $"{Base}/github/profile";
-        public const string GetEvents = $"{Base}/github/events";
-        public const string StorePersonalAccessToken = $"{Base}/github/personal-access-token";
-        public const string RevokePersonalAccessToken = $"{Base}/github/personal-access-token";
     }
 }
